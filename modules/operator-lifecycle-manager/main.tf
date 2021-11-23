31e6838b9a30318001e257c0727b2146bfb04844
Joey Davenport <jungle.bird@gmail.com>
2021-11-23 09:54:13 -0700

    Allow for newer versions of helm provider

--- modules/operator-lifecycle-manager/main.tf
+++ modules/operator-lifecycle-manager/main.tf
@@ -25,7 +25,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.2.0"
+      version = ">=2.2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
--- modules/victoria-metrics/main.tf
+++ modules/victoria-metrics/main.tf
@@ -23,7 +23,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.2.0"
+      version = ">=2.2.0"
     }
   }
 }
